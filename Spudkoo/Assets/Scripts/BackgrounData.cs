6ffe565268de002049f0af5c1d06474b564f4982
Aden Li <149911481+qpalzmwoskxneidjcnrufhvbtygpqlamzowksnx@users.noreply.github.com>
2026-03-13 18:58:31 -0700

    e

---
+++ Spudkoo/Assets/Scripts/BackgrounData.cs
@@ -0,0 +1,11 @@
+using System.IO.Enumeration;
+using UnityEngine;
+[CreateAssetMenu(fileName="NewBackgroundData", menuName="Background/BackgroundData")]
+public class BackgroundData : ScriptableObject
+{
+    public string BackgroundName;
+    public Sprite BackgroundSprite;
+    public int Cost = 0;
+    public bool Unlocked;
+    public int ID;
+}
---
+++ Spudkoo/Assets/Scripts/Background.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class Background : MonoBehaviour
+{
+    public Image BackgroundImage;
+    public TMP_Text BackgroundText;
+
+}
