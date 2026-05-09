8a5d5d18fb6cf0a7af772d66dc284eea0d236005
Aden Li <149911481+qpalzmwoskxneidjcnrufhvbtygpqlamzowksnx@users.noreply.github.com>
2026-05-08 19:35:22 -0700

    backgroundcards

--- Spudkoo/Assets/Scripts/BackgroundBrain.cs
+++ Spudkoo/Assets/Scripts/BackgroundBrain.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
-
+using System.Collections.Generic;
 public class BackgroundBrain : MonoBehaviour
 {
 
@@ -10,14 +10,26 @@ public class BackgroundBrain : MonoBehaviour
 
     [Header("UI References")]
     [SerializeField] private Image backgroundImage;
-
+    public List<BackgroundData> backgrounddata=new();
+    public BackgroundCard cardprefab;
+    public Transform cardContent;
     private void Awake()
     {
         Instance = this;
+        InitializeBackgroundCards();
     }
 
     public void LoadBackground(BackgroundData bgd)
     {
         backgroundImage.sprite = bgd.BackgroundSprite; 
+    }
+    private void InitializeBackgroundCards()
+    {
+        foreach(BackgroundData bd in backgrounddata) 
+        {
+            BackgroundCard bc = Instantiate(cardprefab, cardContent);
+            bc.Initialize(bd);
+        }
+
     }
 }
